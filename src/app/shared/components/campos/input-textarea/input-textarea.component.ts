9521d361bfc78ff5e3f6feae8eb99c832bff1d7f
TioDaniloBR <64714612+TioDaniloBR@users.noreply.github.com>
2020-06-27 17:05:02 -0300

    Criação do formulário e componetização dos inputs

---
+++ src/app/shared/components/campos/input-textarea/input-textarea.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { ValidarCamposService } from '../validar-campos.service';
+import { FormGroup, AbstractControl } from '@angular/forms';
+
+@Component({
+  selector: 'dio-input-textarea',
+  templateUrl: './input-textarea.component.html',
+  styleUrls: ['./input-textarea.component.scss']
+})
+export class InputTextareaComponent {
+  
+  @Input() titulo: string;
+  @Input() formGroup: FormGroup;
+  @Input() controlName: string;
+
+  constructor(public validacao: ValidarCamposService) { }
+
+  get formControl(): AbstractControl {
+    return this.formGroup.controls[this.controlName];
+  }
+}
---
+++ src/app/shared/components/campos/validar-campos.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, ControlContainer } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidarCamposService {
+
+  constructor() { }
+
+  hasErrorValidar(control: AbstractControl, errorName: string): boolean{
+     if((control.dirty || control.touched) && this.hasError(control, errorName)){
+      return true;
+     }else{
+       return false;
+     }
+  }
+
+  hasError(control: AbstractControl, errorName: string): boolean{
+    return control.hasError(errorName);
+  }
+
+  lengthValidar(control: AbstractControl, errorName: string): number{
+    const error = control.errors[errorName];
+    return error.requiredLength || error.min || error.max || 0;
+  }
+}
