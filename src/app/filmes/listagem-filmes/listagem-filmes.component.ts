2383c27a980c92befa69410a84ddb7a661593308
TioDaniloBR <64714612+TioDaniloBR@users.noreply.github.com>
2020-06-28 19:09:10 -0300

    listagem dos resultados melhorada e melhoria de performance

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+
+import { debounceTime } from "rxjs/operators";
+
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
 import { ConfigParams } from 'src/app/shared/models/config-params';
@@ -10,6 +13,7 @@ import { ConfigParams } from 'src/app/shared/models/config-params';
   styleUrls: ['./listagem-filmes.component.scss']
 })
 export class ListagemFilmesComponent implements OnInit {
+  readonly semFoto = "https://www.termoparts.com.br/wp-content/uploads/2017/10/no-image.jpg";
 
   config: ConfigParams = {
     pagina: 0,
@@ -28,7 +32,9 @@ export class ListagemFilmesComponent implements OnInit {
       texto: [''],
       genero: ['']
     });
-    this.filtrosListagem.get('texto').valueChanges.subscribe( (val: string) => {
+    this.filtrosListagem.get('texto').valueChanges
+    .pipe(debounceTime(400))
+    .subscribe( (val: string) => {
       this.config.pesquisa = val;
       this.resetarConsulta();
     });
