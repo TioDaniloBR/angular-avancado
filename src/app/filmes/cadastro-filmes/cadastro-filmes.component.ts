f66c4d66f27116128f31b96bbd946c9c2ed6f482
TioDaniloBR <64714612+TioDaniloBR@users.noreply.github.com>
2020-06-28 20:55:26 -0300

    listando página de cadastro com a função editar ainda não funcional

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 
 import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
@@ -17,6 +17,7 @@ import { Alerta } from 'src/app/shared/models/alerta';
 })
 export class CadastroFilmesComponent implements OnInit {
 
+  id:number;
   cadastro: FormGroup;
   generos: Array<string>;
 
@@ -24,7 +25,8 @@ export class CadastroFilmesComponent implements OnInit {
               public dialog: MatDialog,
               private fb: FormBuilder,
               private filmeService: FilmesService,
-              private router: Router
+              private router: Router,
+              private activatedRoute: ActivatedRoute
              ) { }
 
   get f() {
@@ -32,16 +34,15 @@ export class CadastroFilmesComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params['id'];
 
-    this.cadastro = this.fb.group({
-      titulo: ['',[Validators.required, Validators.minLength(2),Validators.maxLength(256)]],
-      urlFoto: ['', [Validators.minLength(10)]],
-      dtLancamento: ['', [Validators.required]],
-      descricao: [''],
-      nota: [0,[Validators.required, Validators.min(0), Validators.max(10)]],
-      urlIMDb: ['', [Validators.minLength(10)]],
-      genero: ['', [Validators.required]]
-    });
+    if(this.id){
+      this.filmeService.visualizar(this.id).subscribe( (filme: Filme) => this.criarFormulario(filme));
+    }else{
+      this.criarFormulario(this.criarFilmeEmBranco());
+    }
+
+    
 
     this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção cientifica', 'Comédia', 'Drama'];
 
@@ -58,6 +59,32 @@ export class CadastroFilmesComponent implements OnInit {
 
   reiniciarForm():void {
     this.cadastro.reset();
+  }
+
+  private criarFormulario(filme: Filme):void{
+    this.cadastro = this.fb.group({
+      titulo: [filme.titulo,[Validators.required, Validators.minLength(2),Validators.maxLength(256)]],
+      urlFoto: [filme.urlFoto, [Validators.minLength(10)]],
+      dtLancamento: [filme.dtLancamento, [Validators.required]],
+      descricao: [filme.descricao],
+      nota: [filme.nota,[Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDb: [filme.urlIMDb, [Validators.minLength(10)]],
+      genero: [filme.genero, [Validators.required]]
+    });
+  }
+
+  private criarFilmeEmBranco():Filme{
+    return {
+      id: null,
+      titulo: null,
+      urlFoto: null,
+      dtLancamento: null,
+      descricao: null,
+      nota: null,
+      urlIMDb: null,
+      genero: null
+
+    }as Filme;
   }
 
   private salvar(filme: Filme): void{
--- src/app/filmes/visualizar-filmes/visualizar-filmes.component.ts
+++ src/app/filmes/visualizar-filmes/visualizar-filmes.component.ts
@@ -46,6 +46,10 @@ export class VisualizarFilmesComponent implements OnInit {
     });
   }
 
+  editar():void{
+    this.router.navigateByUrl('/filmes/cadastro/' + this.id);
+  }
+
   private visualizar(): void{
     this.filmesService.visualizar(this.id).subscribe( (filme: Filme) => this.filme = filme);
     
